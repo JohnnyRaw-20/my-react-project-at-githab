91a17a619a8206a91fcc8ecda2f898047af80e7e
JohnnyRaw-20 <unikain18@gmail.com>
2020-06-05 15:27:08 +0300

    add other avatar

---
+++ build/precache-manifest.223599447c151f05c13ef74c3909a412.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e228c510838c93817936bd6c4560c40d",
+    "url": "/my-react-project-at-githab/index.html"
+  },
+  {
+    "revision": "32a45a859a715739e301",
+    "url": "/my-react-project-at-githab/static/css/3.cd1e42c9.chunk.css"
+  },
+  {
+    "revision": "bcfcd3841af327645174",
+    "url": "/my-react-project-at-githab/static/css/4.5d486206.chunk.css"
+  },
+  {
+    "revision": "5e10dca1715a36cc8a4a",
+    "url": "/my-react-project-at-githab/static/css/main.0c66de7c.chunk.css"
+  },
+  {
+    "revision": "33b85bbd4a206cbc7ab3",
+    "url": "/my-react-project-at-githab/static/js/2.5eccb03c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/my-react-project-at-githab/static/js/2.5eccb03c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a45a859a715739e301",
+    "url": "/my-react-project-at-githab/static/js/3.6f80bf58.chunk.js"
+  },
+  {
+    "revision": "bcfcd3841af327645174",
+    "url": "/my-react-project-at-githab/static/js/4.9a6b31f8.chunk.js"
+  },
+  {
+    "revision": "5e10dca1715a36cc8a4a",
+    "url": "/my-react-project-at-githab/static/js/main.a81ed7fc.chunk.js"
+  },
+  {
+    "revision": "0631b74ae871748fceb1",
+    "url": "/my-react-project-at-githab/static/js/runtime-main.05aad6e2.js"
+  },
+  {
+    "revision": "ad0acd4d73e0b817f215616eeec5b222",
+    "url": "/my-react-project-at-githab/static/media/hipon_1.ad0acd4d.png"
+  },
+  {
+    "revision": "80f8d2c6151f03121a15b0857cb78792",
+    "url": "/my-react-project-at-githab/static/media/parisAirport.80f8d2c6.jpg"
+  },
+  {
+    "revision": "0ba658a9a3c4627ec27518a448830da5",
+    "url": "/my-react-project-at-githab/static/media/preloader.0ba658a9.svg"
+  }
+]);
